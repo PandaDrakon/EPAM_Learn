d9e3ad62c9d809e7156e042724fc292ac6d79cd6
PandaDrakon <45831978+PandaDrakon@users.noreply.github.com>
2022-01-12 14:31:13 +0200

    Create RevertStringExample.java

---
+++ RevertStringExample.java
@@ -0,0 +1,13 @@
+public class LambdaJava8 {
+    public static void main(String[] args) {
+        example1();
+    }
+
+    private static void example1() {
+        String source = "Revert this message in this example";
+        StringBuilder reversed = new StringBuilder();
+        for (int i = source.length() - 1 ; i > 0 ; i--) {
+            reversed.append(source.charAt(i));
+        }
+    }
+}
