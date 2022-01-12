70734fd0edc600a0df5d0af24737363f435acf35
PandaDrakon <45831978+PandaDrakon@users.noreply.github.com>
2022-01-12 14:52:01 +0200

    Update RevertStringExample.java

--- RevertStringExample.java
+++ RevertStringExample.java
@@ -1,13 +1,11 @@
 public class RevertStringClass {
-       public static void main(String[] args) {
-        String source = "Revert this message in this example";
-           
+    public static void main(String[] args) {
+        String source = "Revert this message in this example";   
         revertStringFunction(source);
     }
-
     private static void revertStringFunction(String source) {
         StringBuilder reversed = new StringBuilder();
-        for (int i = source.length() - 1 ; i > 0 ; i--) {
+        for (int i = source.length() - 1 ; i >= 0 ; i--) {
             reversed.append(source.charAt(i));
         }
     }
